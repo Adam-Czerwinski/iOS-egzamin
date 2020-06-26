0090d6e3bf12145c3626910a1a6ce0abf23bbb68
Adam-Czerwinski <adam.czerwinski097@gmail.com>
2020-06-26 12:49:23 +0200

    'finish'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Md5 } from 'md5-typescript';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  kodWynikowy: string = undefined;
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() {}
-
+  onSend(imie: string, nazwisko: string): void {
+    this.httpClient
+      .get(
+        `http://sroczynski.pl/iosexamrest/examresult/${Md5.init(
+          `${nazwisko} ${imie}`
+        )}`
+      )
+      .subscribe((response) => (this.kodWynikowy = response['result']));
+  }
 }
